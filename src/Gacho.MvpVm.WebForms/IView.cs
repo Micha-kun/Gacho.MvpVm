cd7c997a25b8e0b0beac8b084efe305286d4c188
Michael Gómez Campos <michakun@gmail.com>
2015-06-14 11:36:49 +0200

    Commit Fixes

---
+++ src/Gacho.MvpVm.WebForms/IView.cs
@@ -0,0 +1,15 @@
+﻿using Gacho.MvpVm.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+
+namespace Gacho.MvpVm.WebForms
+{
+    public interface IView<out TModel> : INamingContainer
+        where TModel : class, IViewModel
+    {
+        TModel Model { get; }
+    }
+}
