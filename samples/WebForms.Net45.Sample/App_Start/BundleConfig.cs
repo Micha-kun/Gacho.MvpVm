a846264533478b7910d1300cc8b518b878c36ee8
Michael Gómez Campos <michakun@gmail.com>
2015-06-20 08:26:15 +0200

    Added Initializr template on sample (WebForms 4.5)

---
+++ samples/WebForms.Net45.Sample/App_Start/BundleConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace WebForms.Net45.Sample
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                         "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                         "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/styles/bootstrap").Include(
+                         "~/Content/bootstrap.css", "~/Content/bootstrap-theme.css"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr-respond").Include(
+                         "~/Scripts/modernizr-{version}.js", "~/Scripts/respond.js"));
+
+            // Code removed for clarity.
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
